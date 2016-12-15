d9e27395f6ca5522086b71b2dfa5c1a20337457a
Genevieve Conty <gmireillec@Genevieves-MacBook-Pro.local>
2016-12-15 11:00:07 -0500

    note related functions

--- public/app.js
+++ public/app.js
@@ -6,7 +6,51 @@ $.getJSON("/articles", function(data){
 })
 
 //grab note on click
+$(document).on("click", "p", function(){
+	
+	//empty note space, get id, call for article
+	$("#notes").empty();
+	var disID = $(this).attr("id");
+	$.ajax({
+		method: "GET",
+		url: "/articles/" + disID
+	})
+		.done(function(data){
+			console.log(data);
+			$("#notes").append("<h2>" + data.title + "</h2>");
+			// inputs
+			$("#notes").append("<input id='titleinput' name='title' >");
+			$("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+			$("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
 
-//display note info
+			// check for exisitig note
+			if (data.note) {
+				$("#titleinput").val(data.note.title);
+				$("#bodyinput").val(data.note.body);
+      		}
+		})
+})
 
-//save button on note
+//save note button
+$(document).on("click", "#savenote", function() {
+// Grab the id associated with the article
+	var disId = $(this).attr("id");
+	$.ajax({
+		method: "POST",
+		url: "/articles/" + thisId,
+		data: {
+		  // Value taken from title input
+		  title: $("#titleinput").val(),
+		  // Value taken from note textarea
+		  body: $("#bodyinput").val()
+		}
+	})
+		.done(function(data) {
+		  console.log(data);
+		  $("#notes").empty();
+		});
+
+	//remove the values entered in the inputs
+	$("#titleinput").val("");
+	$("#bodyinput").val("");
+});
